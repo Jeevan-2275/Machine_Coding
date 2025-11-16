38f0613a265b5db5882aac129844c2d038207f40
Jeevan-2275 <jeevankadam2275@gmail.com>
2025-11-17 00:04:19 +0530

    Update max_Subarray.cpp implementation

--- CPP/Daily Question/Day-2/max_Subarray.cpp
+++ CPP/Daily Question/Day-2/max_Subarray.cpp
@@ -1,1 +1,22 @@
 // 11.maximum subarray sum.
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int max_so_far = arr[0];
+    int max_ending_here = arr[0];
+
+    for(int i=1;i<n;i++){
+        max_ending_here = max(arr[i], max_ending_here + arr[i]);
+        max_so_far = max(max_so_far, max_ending_here);
+    }
+
+    cout<<"Maximum Subarray Sum: "<<max_so_far<<endl;
+
+    return 0;
+}

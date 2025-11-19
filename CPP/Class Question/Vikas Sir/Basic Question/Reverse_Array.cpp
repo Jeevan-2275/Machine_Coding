c48b51690dce4889172a0b63769b25ad306c348c
Jeevan-2275 <jeevankadam2275@gmail.com>
2025-11-20 00:00:03 +0530

    Added  A question

---
+++ CPP/Class Question/Vikas Sir/Basic Question/Reverse_Array.cpp
@@ -0,0 +1,104 @@
+// 1. Reverse an array
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+   int arr[] = {1, 2, 3, 4, 5};
+   int n = sizeof(arr)/sizeof(arr[0]);
+   for(int i = n - 1; i >= 0; i--){
+       cout << arr[i] << " ";
+   }
+    return 0;
+}
+
+1. Reverse an array
+2.  check if a number in prime or not.
+3.  check  if a number is palindrome or not.
+4.  check if a Array Is Sorted.
+5. Find index of target in an array.
+6. Find Index of target in an array.
+7. Point Frequency of each element.
+8. Reverse a String (two pointer)
+9. Find Missing number in 1 to n.
+10. check if string is Anagram.
+11. Implement Stack using  Array.
+12. Implement Queue using array.
+13. Return  Intersection of two array.
+
+
+35.print Fibonacci series.
+36.find n Fibonacci number.
+37.count digit in a number
+38.check if a given num is Armstrong
+39.count positive and negative and zero 144
+40. *
+    * *
+    * * *
+    * * * *
+
+41. * * * *
+    * * *
+    * *
+    *
+
+42. full pyramid
+    * 
+   * *
+ *  *  *
+*  *  * *
+
+43.
+
+* * * *
+ * * *
+  * *
+   *
+
+44.
+    1
+    0 1
+    1 0 1
+    0 1 0 1
+    1 0 1 0
+
+45. hollow square pattern
+* * * *
+*     *
+*     *
+* * * *
+
+
+46. hollow pyramid
+    *
+   * *
+  *   *
+ *     *
+* * * * *
+
+47. diamond pattern
+           *
+         * * *
+        * * * *
+       * * * * *
+        * * * *
+         * * *
+          * *
+           *
+ 
+ 49. hollow diamond
+          *
+        *   *
+      *       *
+    *           *
+      *       *
+        *   *
+          *
+
+50. pascal's triangle
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1    

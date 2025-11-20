9e5010ef2de869adf613e268d9de0504f4f0c137
Jeevan-2275 <jeevankadam2275@gmail.com>
2025-11-20 23:52:53 +0530

    feat: add logic to find the k-th largest element in k'th_large.cpp

--- CPP/Daily Question/Day-4/k'th_large.cpp
+++ CPP/Daily Question/Day-4/k'th_large.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 int main(){
-     int arr[] = {3,2,1,5,6,4};
+     int arr[] = {3,2,1,5,6,4};  
      int n = sizeof(arr)/sizeof(arr[0]);
      int k = 2;
         vector<int> vec;

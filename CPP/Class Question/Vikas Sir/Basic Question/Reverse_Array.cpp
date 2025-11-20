8daf34e5af869bcce88f9871c15e009f940fb90e
Jeevan-2275 <jeevankadam2275@gmail.com>
2025-11-20 23:52:42 +0530

    feat: implement array reversal logic in Reverse_Array.cpp

--- CPP/Class Question/Vikas Sir/Basic Question/Reverse_Array.cpp
+++ CPP/Class Question/Vikas Sir/Basic Question/Reverse_Array.cpp
@@ -13,19 +13,19 @@ int main(){
     return 0;
 }
 
-1. Reverse an array
-2.  check if a number in prime or not.
-3.  check  if a number is palindrome or not.
-4.  check if a Array Is Sorted.
-5. Find index of target in an array.
-6. Find Index of target in an array.
-7. Point Frequency of each element.
-8. Reverse a String (two pointer)
-9. Find Missing number in 1 to n.
-10. check if string is Anagram.
-11. Implement Stack using  Array.
-12. Implement Queue using array.
-13. Return  Intersection of two array.
+// 1. Reverse an array
+// 2.  check if a number in prime or not.
+// 3.  check  if a number is palindrome or not.
+// 4.  check if a Array Is Sorted.
+// 5. Find index of target in an array.
+// 6. Find Index of target in an array.
+// 7. Point Frequency of each element.
+// 8. Reverse a String (two pointer)
+// 9. Find Missing number in 1 to n.
+// 10. check if string is Anagram.
+// 11. Implement Stack using  Array.
+// 12. Implement Queue using array.
+// 13. Return  Intersection of two array.
 
 
 35.print Fibonacci series.
@@ -96,9 +96,11 @@ int main(){
         *   *
           *
 
+          
 50. pascal's triangle
 1
 1 1
 1 2 1
 1 3 3 1
-1 4 6 4 1    
+1 4 6 4 1   
+

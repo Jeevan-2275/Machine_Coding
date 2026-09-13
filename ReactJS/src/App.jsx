de2e60d7e5eb6a63b7db50e3789cf1409704e503
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-09-14 00:03:33 +0530

    chore: update app component

--- ReactJS/src/App.jsx
+++ ReactJS/src/App.jsx
@@ -1,5 +1,7 @@
-import  SearchList from './components/2.Searchable List/Searchable.jsx';
+// import  SearchList from './components/2.Search List/Search.jsx';
 // import Counter from './components/1.counter/counter.jsx';
+import TodoList from './components/3.Implement a simple Todo app with add and remove/Todo.jsx';
+
 import './App.css'
 
 const  App = () => {
@@ -8,7 +10,8 @@ const  App = () => {
   return (
    <div>
     {/* <Counter /> */}
-    <SearchList />
+    {/* <SearchList /> */}
+    <TodoList />
    </div>
   )
 }

f541fc072d62af5ebd498b8c07fc0b30dee01bc9
Jeevan-2275 <jeevankadam2275@gmail.com>
2026-09-18 01:32:16 +0530

    feat: update App component

--- ReactJS/src/App.jsx
+++ ReactJS/src/App.jsx
@@ -3,6 +3,7 @@
 // import TodoList from './components/3.Implement a simple Todo app with add and remove/Todo.jsx';
 import ColorChange from './components/4. Create Button that change color when clicked/ColorChange.jsx';
 import './App.css'
+import Form from './components/5.Build a form with multiple fields and validation. (useState)/Form.jsx';
 
 const  App = () => {
 
@@ -12,7 +13,8 @@ const  App = () => {
     {/* <Counter /> */}
     {/* <SearchList /> */}
     {/* <TodoList /> */}
-    { <ColorChange /> }
+    {/* { <ColorChange /> } */}
+    <Form />
    </div>
   )
 }
